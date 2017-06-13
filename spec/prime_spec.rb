2e9eb01235271516aa3206bd43a13eee5d9ac979
Gideon <gideonj1@gmail.com>
2017-06-13 16:26:42 -0400

    addin spec_helper

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -1,7 +1,18 @@
 require './lib/prime'
+require 'spec_helper'
 
 describe Prime do
-  it "should be true" do
-    Prime.new().should be_true
+
+  before(:each) do
+    @prime = Prime.new()
+    @order.calc_table
+    @m = @prime.m
+    @n = @prime.n
   end
+
+  subject { @prime }
+  it { should be_valid}
+  it { should respond_to(:n) }
+  it { should respond_to(:m) }
 end
+  
